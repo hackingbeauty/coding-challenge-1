87adb3d2c555b7ec009afd3c3d28bc38ca3534d9
Mark Muskardin <markmuskardin@gmail.com>
2011-05-16 15:35:20 -0700

    working on google maps integration

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,15 +15,26 @@ $(document).ready (function() {
                                       '<div>' + restaurants[r].vicinity + '</div>' +
                                       '</li>');
           }
-          MonkeyInferno.drawGoogleMap();
+          MonkeyInferno.drawGoogleMap(restaurants);
         },
         error:function(res){
           alert("There was an error!");
         }
       })
     },
-    drawGoogleMap:function(){
-      console.log("inside drawMapPoints");
+    drawGoogleMap:function(restaurants){
+      var map = new GMap2(document.getElementById("map_canvas"));
+      map.setCenter(new GLatLng(37.77493,-122.41942), 13);
+      for(r in restaurants){
+        // console.log("restaurants are " + restaurants[r].name + " " + restaurants[r].geometry.location.lat);
+        var point = new GLatLng(restaurants[r].geometry.location.lat, restaurants[r].geometry.location.long);
+        var marker = new GMarker(point);
+        GEvent.addListener(marker, "click", function() {
+          map.openInfoWindowHtml(marker);
+        }); 
+        map.addOverlay(marker);
+        
+      }
     }
   }
   
