7b8919eb9af2430f99f53bc94072e47e38e8ccf6
Mark Muskardin <markmuskardin@gmail.com>
2011-05-16 14:52:52 -0700

    js tweak

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,22 +6,24 @@ $(document).ready (function() {
         url: '/get_restaurants',
         dataType: 'json',
         success:function(res){
-          console.log(res);
           var restaurants = res.results;
+          console.log(restaurants);
           for(r in restaurants){
-            console.log(restaurants[r]);
             $('#restaurants').append( '<li>' + 
-                                        '<img src=' + restaurants[r].icon +' alt=' + restaurants[r].name + '/>' + 
-                                        '<span>' + restaurants[r].name + '</span>' +
-                                        '<div>' + restaurants[r].vicinity + '</div>' +
+                                      '<img src=' + restaurants[r].icon +' alt=' + restaurants[r].name + '/>' + 
+                                      '<span>' + restaurants[r].name + '</span>' +
+                                      '<div>' + restaurants[r].vicinity + '</div>' +
                                       '</li>');
           }
-          return res;
+          MonkeyInferno.drawGoogleMap();
         },
         error:function(res){
           alert("There was an error!");
         }
       })
+    },
+    drawGoogleMap:function(){
+      console.log("inside drawMapPoints");
     }
   }
   
