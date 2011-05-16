00550d51e435c1438695e85f1a34e352a52dacaa
Mark Muskardin <markmuskardin@gmail.com>
2011-05-16 14:07:18 -0700

    successfully getting JSON response from Google Places

--- monkey_inferno.rb
+++ monkey_inferno.rb
@@ -2,6 +2,11 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'yaml'
+require 'open-uri'
+require 'openssl'
+require 'json'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE #SSL open-uri hack. open-uri can't read ssl links
 
 ### Configuration
 configure do   
@@ -12,7 +17,8 @@ configure do
 end
 
 get '/' do
-  
+  url = 'https://' + settings.san_francisco_http_call + "&key=" + settings.google_places_api_key
+  @jsonResponse = open(url).read.to_s
   haml :index
 end
 
