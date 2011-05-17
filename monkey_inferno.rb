03f963407b8e9c546bd7146ad7a702285cd2c518
Mark Muskardin <markmuskardin@gmail.com>
2011-05-16 23:22:55 -0700

    added autocomplete

--- monkey_inferno.rb
+++ monkey_inferno.rb
@@ -29,8 +29,10 @@ end
 get '/autocomplete_search' do
   input = URI.escape params[:searchInput]
   puts "input is #{input}"
-  url = settings.google_places_autocomplete_url + 'input=' + input +'&types=geocode&sensor=false&key=' + settings.google_places_api_key
+  url = settings.google_places_autocomplete_url + 'input=' + input +'&types=food&sensor=false&key=' + settings.google_places_api_key
+  jsonResponse = open(url).read.to_s
   puts url
+  return jsonResponse
 end
 
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,59 +1,89 @@
 $(document).ready (function() {
 
   MonkeyInferno = {
+    map: undefined,
     init:function(){
-      this.getRestaurants();
-      this.searchOnKeyUp();
+      this.drawMap();
+      this.getRestaurantList();
+      this.autoComplete();
     },
-    searchOnKeyUp: function(){
-      $('input#searchInput').keyup(function(e){
-        $.ajax({
-          type: 'get',
-          url: '/autocomplete_search',
-          data: $('#searchForm').serialize(),
-          dataType: 'json',
-          error: function(res){
-            
-          },
-          success: function(res){
-            
-          }
-        });
+    drawMap: function(){
+      var sf = new google.maps.LatLng(37.77493,-122.41942);
+      MonkeyInferno.map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: sf,
+        zoom: 12
+      });
+      var request = {
+        location: sf,
+        //Google says this value is set in meters. San Francisco is 47.6 square miles, 
+        //which is 123 Kilometers, which is 123,000 meters; however, the generated points exceed the boundary of SF
+        radius: '12000', 
+        types: ['restaurant']
+      };
+      infowindow = new google.maps.InfoWindow();
+      service = new google.maps.places.PlacesService(MonkeyInferno.map);
+      service.search(request, MonkeyInferno.mapCallBack);
+    },
+    mapCallBack:function(results,status){
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          MonkeyInferno.createMarker(place); 
+        }
+      }
+    },
+    createMarker:function(place){
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: MonkeyInferno.map,
+        position: new google.maps.LatLng(placeLoc.lat(), placeLoc.lng())
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
       });
     },
-    getRestaurants:function(){
-      $.ajax({
-        url: '/get_restaurants',
-        dataType: 'json',
-        success:function(res){
-          var restaurants = res.results;
-          // console.log(restaurants);
-          for(r in restaurants){
-            $('#restaurants').append( '<li>' + 
-                                      '<img src=' + restaurants[r].icon +' alt=' + restaurants[r].name + '/>' + 
-                                      '<span>' + restaurants[r].name + '</span>' +
-                                      '<div>' + restaurants[r].vicinity + '</div>' +
-                                      '</li>');
-          }
-          MonkeyInferno.drawGoogleMap(restaurants);
-        },
-        error:function(res){
-          alert("There was an error!");
-        }
-      })
+    autoComplete:function(){
+      var bounds, southWestLat, southWestLong, northEastLat, northEastLong;
+      google.maps.event.addListenerOnce(MonkeyInferno.map, 'bounds_changed', function(){
+         bounds = this.getBounds();
+         southWestLat = bounds.getSouthWest().Ja;
+         southWestLong = bounds.getSouthWest().Ka;
+         northEastLat = bounds.getNorthEast().Ja;
+         northEastLong = bounds.getNorthEast().Ka;
+
+         var defaultBounds = new google.maps.LatLngBounds(
+           new google.maps.LatLng(southWestLat,southWestLong),
+           new google.maps.LatLng(northEastLat,northEastLong)
+         );
+         var input = document.getElementById('searchInput');
+         var options = {
+           bounds: defaultBounds
+         };
+
+         autocomplete = new google.maps.places.Autocomplete(input, options);
+      });
+
     },
-    drawGoogleMap:function(restaurants){
-      var map = new GMap2(document.getElementById("map_canvas"));
-      map.setCenter(new GLatLng(37.77493,-122.41942), 13);
-      for(r in restaurants){
-        // console.log("restaurants are " + restaurants[r].name + " " + restaurants[r].geometry.location.lat);
-        var point = new GLatLng(restaurants[r].geometry.location.lat, restaurants[r].geometry.location.long);
-        var marker = new GMarker(point);
-        GEvent.addListener(marker, "click", function() {
-          map.openInfoWindowHtml(marker);
-        }); 
-        map.addOverlay(marker);
-      }
+    getRestaurantList:function(){
+      // $.ajax({
+      //   url: '/get_restaurants',
+      //   dataType: 'json',
+      //   success:function(res){
+      //     var restaurants = res.results;
+      //     for(r in restaurants){
+      //       $('#restaurants').append( '<li>' + 
+      //                                 '<img src=' + restaurants[r].icon +' alt=' + restaurants[r].name + '/>' + 
+      //                                 '<span>' + restaurants[r].name + '</span>' +
+      //                                 '<div>' + restaurants[r].vicinity + '</div>' +
+      //                                 '</li>');
+      //     }
+      //   },
+      //   error:function(res){
+      //     alert("There was an error!");
+      //   }
+      // })
     }
   }
   	
